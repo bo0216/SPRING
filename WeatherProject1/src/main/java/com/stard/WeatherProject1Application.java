c30ca06f57c3121c27aac3382c496fbec90c4428
bok <bo216@naver.com>
2019-06-09 17:34:47 +0900

    weatherProject

---
+++ WeatherProject1/src/main/java/com/stard/WeatherProject1Application.java
@@ -0,0 +1,13 @@
+package com.stard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherProject1Application.class, args);
+	}
+
+}
